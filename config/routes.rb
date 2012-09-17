0a869fc4c815318e727c32433e96348c72f772dd
Matthew Powers <matthewkevinpowers@gmail.com>
2012-09-16 20:27:02 -0400

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
+  root :to => 'pages#home'
+  match '/about', :to => 'pages#about'  #matches about path to about action in controller
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
